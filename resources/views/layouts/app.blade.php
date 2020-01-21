4a4228baa9c5fbda3a9ea0ea45c62a89291a3ff2
harib128 <tomshaddock@googlemail.com>
2020-01-21 18:11:26 +0100

    Add the files for saas

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Todos list in Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
+
+  <footer class="text-center">Copyright (c) 2020 Copyright Holder All Rights Reserved.</footer>
+</html>
